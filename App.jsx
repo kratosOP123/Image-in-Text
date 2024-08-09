e89a0d3ea13faef47a655802dbc9b37b8deea237
kratosOP123 <164128468+kratosOP123@users.noreply.github.com>
2024-08-09 17:04:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-full bg-black'>
+      <div className='flex items-center justify-center h-screen'>
+        <p className='image text-[250px]'>IO TEAM</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+// @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+
+// /* Reset styles */
+// * {
+//     padding: 0;
+//     margin: 0;
+//   /*   box-sizing: border-box; */
+//   }
+  
+// .image{
+//     background-image: url("https://cdn.pixabay.com/photo/2024/07/17/11/14/sunrise-8901272_640.jpg");
+//     background-size: cover;
+//     background-clip: text;
+//     -webkit-background-clip: text;
+//     color: transparent;
+// }
+
+// @layer base{
+//     html {
+//         font-family: 'Cascadia Code'
+//     }
+
+//     body{
+//         background-color: black;
+//         color: white;      
+//     }
+// }
